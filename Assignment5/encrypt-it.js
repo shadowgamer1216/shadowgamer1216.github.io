1efa2b90382c0fdadb771c4271369e382b7e9f2b
shadowgamer1216 <meamill88@gmail.com>
2022-12-05 22:38:53 -0800

    Initial commit of Assignment 5 starter files

---
+++ Assignment5/encrypt-it.js
@@ -0,0 +1,26 @@
+/*
+ * Starter file 
+ */
+(function() {
+  "use strict";
+
+  /**
+   * The starting point in our program, setting up a listener
+   * for the "load" event on the window, signalling the HTML DOM has been constructed
+   * on the page. When this event occurs, the attached function (init) will be called.
+   */
+  window.addEventListener("load", init);
+
+  /**
+   * TODO: Write a function comment using JSDoc.
+   */
+  function init() {
+    // Note: In this function, we usually want to set up our event handlers
+    // for UI elements on the page.
+  }
+
+  // Add any other functions in this area (you should not implement your
+  // entire program in the init function, for similar reasons that
+  // you shouldn't write an entire Java program in the main method).
+
+})();
